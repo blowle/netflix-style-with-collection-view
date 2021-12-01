20ef696e94fd6e0ff9f34f0b03be23e1b8282b45
Yongcheol Lee <dydcjf8889@gmail.com>
2021-12-01 16:27:36 +0900

    configure naivgationbar

--- NetflexStyleWithCollectionView/HomeViewController.swift
+++ NetflexStyleWithCollectionView/HomeViewController.swift
@@ -11,5 +11,14 @@ class HomeViewController: UICollectionViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // navigation setting
+        navigationController?.navigationBar.backgroundColor = .clear
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationController?.navigationBar.shadowImage = UIImage()
+        navigationController?.hidesBarsOnSwipe = true
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "netflix_icon"), style: .plain, target: nil, action: nil)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "person.crop.circle"), style: .plain, target: nil, action: nil)
     }
 }
