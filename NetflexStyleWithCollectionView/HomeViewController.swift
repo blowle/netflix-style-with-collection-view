3c8f1e683d3c065e1dbc02e3f3b1819a3b96b136
Yongcheol Lee <dydcjf8889@gmail.com>
2021-12-01 16:19:12 +0900

    configure root view controller

--- NetflexStyleWithCollectionView/HomeViewController.swift
+++ NetflexStyleWithCollectionView/HomeViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UICollectionViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
--- NetflexStyleWithCollectionView/SceneDelegate.swift
+++ NetflexStyleWithCollectionView/SceneDelegate.swift
@@ -13,10 +13,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = scene as? UIWindowScene else { return }
+        window = UIWindow(windowScene: windowScene)
+        
+        let layout = UICollectionViewFlowLayout()
+        let homeVC = HomeViewController(collectionViewLayout: layout)
+        let rootNavigationController = UINavigationController(rootViewController: homeVC)
+        
+        window?.rootViewController = rootNavigationController
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
